47083312c02906d3223e7b3b99be6d49e4fb9589
Murali <murali.dk.reddy@gmail.com>
2016-09-27 22:00:43 -0400

    initial commit

---
+++ shopping-eurekaserver/src/main/java/com/spring/cloud/eurekaserver/ShoppingEurekaserverApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingEurekaserverApplication.class, args);
+	}
+}
