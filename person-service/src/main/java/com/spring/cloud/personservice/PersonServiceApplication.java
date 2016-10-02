52e9d68827384cc74bfa103cc2acc6c38e9e1461
Murali <murali.dk.reddy@gmail.com>
2016-10-02 17:41:32 -0400

    adding confing .properties file

--- person-service/src/main/java/com/spring/cloud/personservice/PersonServiceApplication.java
+++ person-service/src/main/java/com/spring/cloud/personservice/PersonServiceApplication.java
@@ -2,7 +2,13 @@ package com.spring.cloud.personservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class PersonServiceApplication {
 
